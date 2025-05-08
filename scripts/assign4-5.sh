2f3609a6223c6ac2db9ffb4676bb62fbf74d1cc8
23Yamaguchi <ut-sapphire@g.ecc.u-tokyo.ac.jp>
2025-05-08 13:30:55 +0900

    課題4.5

--- scripts/assign4-5.sh
+++ scripts/assign4-5.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sox ./data/doremi.wav -t raw -c 1 - | ./build/bandpass 8192 300 3400 |\
+sox ./data/doremi.wav -t raw -c 1 - | ./build/bandpass 8192 500 10000 |\
  play -t raw -b 16 -c 1 -e s -r 44100 -
