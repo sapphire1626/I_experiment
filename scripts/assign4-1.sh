39980ec32f8068e3cda4454286d220b8fca65075
23yamaguchi <ut-sapphire@g.ecc.u-tokyo.ac.jp>
2025-05-17 17:15:33 +0900

    downsampleを修正

---
+++ scripts/assign4-1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sox ./data/obama.wav -t raw -b 16 -c 1 -e s -r 44100 - |\
+./build/downsample "$1" | play -t raw -b 16 -c 1 -e s -r $((44100 / $1)) -
--- src/downsample.c
+++ src/downsample.c
@@ -4,13 +4,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define N 256
-
-void die(char *s) {
-  perror(s);
-  exit(1);
-}
-
+/// @brief
+/// 標準入力から音声データを読み込み、指定されたインターバルでダウンサンプリングして標準出力に書き出す
+/// @param argv[1]: インターバル
 /// @example sox ./data/obama.wav -t raw -b 16 -c 1 -e s -r 44100 - |
 /// ./build/downsample 5 | play -t raw -b 16 -c 1 -e s -r 8820 -
 int main(int argc, char **argv) {
@@ -19,26 +15,21 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  int rate = atoi(argv[1]);
+  const int rate = atoi(argv[1]);
 
-  int16_t buf[N];
-
+  int16_t buf;
   int n;
-  int i = 0;
-  while (1) {
-    n = read(STDIN_FILENO, buf, 2);
+  int count = 0;
+  while ((n = read(STDIN_FILENO, &buf, sizeof(int16_t))) > 0) {
+    if (count % rate == 0) {
+      int _ = write(STDOUT_FILENO, &buf, 2);
+    }
+    ++count;
+  }
 
-    if (n == -1) {
-      die("read");
-    }
-    if (n == 0) {
-      break;
-    }
-
-    if (i % rate == 0) {
-      int _ = write(STDOUT_FILENO, &buf[0], 2);
-    }
-    ++i;
+  if (n < 0) {
+    perror("read");
+    exit(1);
   }
 
   return 0;
