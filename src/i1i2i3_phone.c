8a8fd95f4bdef7879ed07ac7b4f9ceccd41b6a9d
23Yamaguchi <ut-sapphire@g.ecc.u-tokyo.ac.jp>
2025-05-27 15:56:19 +0900

    同時読み書きできんの〜？

--- src/i1i2i3_phone.c
+++ src/i1i2i3_phone.c
@@ -38,20 +38,33 @@ void finish(const char* cmd) {
 ///@param argv[2] 通信先ポート番号
 ///@param argv[3] 受信ポート番号
 int main(int argc, char* argv[]) {
-  const char* ip_addr = argv[1];
-  const int recv_port = atoi(argv[2]);
-  const int send_port = atoi(argv[3]);
-  if (argc != 4) {
-    printf("Usage: %s <ip_addr> <recv_port> <send_port>\n", argv[0]);
-    return 1;
+  //   const char* ip_addr = argv[1];
+  //   const int recv_port = atoi(argv[2]);
+  //   const int send_port = atoi(argv[3]);
+  //   if (argc != 4) {
+  //     printf("Usage: %s <ip_addr> <recv_port> <send_port>\n", argv[0]);
+  //     return 1;
+  //   }
+
+  struct sockaddr_in addr;
+    socklen_t addr_len = sizeof(struct sockaddr_in);
+  //   s_recv = setUpSocketTcp(&addr_recv, ip_addr, recv_port);
+
+  //   struct sockaddr_in addr_send;
+  //   socklen_t addr_len_send = sizeof(struct sockaddr_in);
+  //   s_send = setUpSocketTcpServer(&addr_send, &addr_len_send, send_port);
+  int s;
+  if (argc == 2) {
+    const int port = atoi(argv[1]);
+    s = setUpSocketTcpServer(&addr, &addr_len, port);
+  } else if (argc == 3) {
+    const char* ip_addr = argv[1];
+    const int port = atoi(argv[2]);
+    s = setUpSocketTcp(&addr, ip_addr, port);
+  } else {
+    printf("Usage: %s <port> or %s <ip_addr> <port>\n", argv[0], argv[0]);
+    exit(EXIT_FAILURE);
   }
-
-  struct sockaddr_in addr_recv;
-  s_recv = setUpSocketTcp(&addr_recv, ip_addr, recv_port);
-
-  struct sockaddr_in addr_send;
-  socklen_t addr_len_send = sizeof(struct sockaddr_in);
-  s_send = setUpSocketTcpServer(&addr_send, &addr_len_send, send_port);
 
   char* cmdline = "rec -t raw -b 16 -c 1 -e s -r 44100 -";
   if ((fp = popen(cmdline, "r")) == NULL) {
@@ -69,13 +82,13 @@ int main(int argc, char* argv[]) {
     }
 
     if (c > 0) {
-      if (write(s_send, buf, c) < 0) {
+      if (write(s, buf, c) < 0) {
         finish("write");
       }
     }
 
     // 受信
-    c = read(s_recv, buf, sizeof(buf));
+    c = read(s, buf, sizeof(buf));
     if (c == 0) {
       break;
     }
